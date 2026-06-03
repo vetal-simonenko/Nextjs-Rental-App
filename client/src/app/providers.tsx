73fb01f3181cfcc26c4b06656b78c27d5871aceb
VitaliiSimonenko <Vitalii_Simonenko@epam.com>
2026-06-03 11:03:05 +0300

    Commit Message

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import StoreProvider from "@/state/redux";
-//import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
 import Auth from "./(auth)/authProvider";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-      {/*<Authenticator.Provider>*/}
-      <Auth>{children}</Auth>
-      {/*</Authenticator.Provider>*/}
+      <Authenticator.Provider>
+        <Auth>{children}</Auth>
+      </Authenticator.Provider>
     </StoreProvider>
   );
 };
